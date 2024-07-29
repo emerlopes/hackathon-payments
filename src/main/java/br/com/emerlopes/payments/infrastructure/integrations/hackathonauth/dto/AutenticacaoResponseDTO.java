b0abaf1a60787dc94a60e292697a7af3513352ee
Emerson <lopes.ds@live.com>
2024-07-29 12:53:52 -0300

    chore: implementando JsonCreator

--- src/main/java/br/com/emerlopes/payments/infrastructure/integrations/hackathonauth/dto/AutenticacaoResponseDTO.java
+++ src/main/java/br/com/emerlopes/payments/infrastructure/integrations/hackathonauth/dto/AutenticacaoResponseDTO.java
@@ -1,5 +1,6 @@
 package br.com.emerlopes.payments.infrastructure.integrations.hackathonauth.dto;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Data;
@@ -9,4 +10,11 @@ import lombok.Data;
 public class AutenticacaoResponseDTO {
     @JsonProperty("token_acesso")
     private String tokenAcesso;
+
+
+    @JsonCreator
+    public AutenticacaoResponseDTO(@JsonProperty("token_acesso") String tokenAcesso) {
+        this.tokenAcesso = tokenAcesso;
+    }
+
 }
