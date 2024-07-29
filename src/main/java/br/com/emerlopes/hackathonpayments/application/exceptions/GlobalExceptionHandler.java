d283e78140242064408899a17f7b4e3fbb51054a
Emerson <lopes.ds@live.com>
2024-07-29 11:12:16 -0300

    chore: implementando excecao personalizada

--- src/main/java/br/com/emerlopes/hackathonpayments/application/exceptions/GlobalExceptionHandler.java
+++ src/main/java/br/com/emerlopes/hackathonpayments/application/exceptions/GlobalExceptionHandler.java
@@ -7,13 +7,16 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.context.request.WebRequest;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(ConstraintViolationException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    protected ResponseEntity<String> handleConstraintViolationException(ConstraintViolationException ex) {
+    protected ResponseEntity<String> handleConstraintViolationException(
+            final ConstraintViolationException ex
+    ) {
         StringBuilder message = new StringBuilder();
         ex.getConstraintViolations().forEach(violation -> message.append(violation.getMessage()).append("; "));
         return new ResponseEntity<>(message.toString(), HttpStatus.BAD_REQUEST);
@@ -21,8 +24,17 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(ErroDeNegocioException.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    protected ResponseEntity<String> handleBusinessException(ErroDeNegocioException ex) {
+    protected ResponseEntity<String> handleBusinessException(
+            final ErroDeNegocioException ex
+    ) {
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(InvalidTokenException.class)
+    public ResponseEntity<Object> handleInvalidTokenException(
+            final InvalidTokenException ex, WebRequest request
+    ) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.UNAUTHORIZED);
     }
 
 }
