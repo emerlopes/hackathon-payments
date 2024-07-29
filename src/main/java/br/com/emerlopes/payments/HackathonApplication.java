861269e8a381ae90cb52dd0778061a14d96f5d9e
Emerson <lopes.ds@live.com>
2024-07-29 12:12:53 -0300

    chore: adicionando anotacao EnableMethodSecurity

--- src/main/java/br/com/emerlopes/payments/HackathonApplication.java
+++ src/main/java/br/com/emerlopes/payments/HackathonApplication.java
@@ -3,8 +3,10 @@ package br.com.emerlopes.payments;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
 @SpringBootApplication
+@EnableMethodSecurity
 @EnableFeignClients({"br.com.emerlopes.payments.infrastructure.integrations*"})
 public class HackathonApplication {
 
