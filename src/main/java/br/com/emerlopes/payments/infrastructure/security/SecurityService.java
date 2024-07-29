8b332b9a9cc985cec202463f7c6e05299d86f27a
Emerson <lopes.ds@live.com>
2024-07-29 12:54:16 -0300

    refact: definindo retorno da variavel

--- src/main/java/br/com/emerlopes/payments/infrastructure/security/SecurityService.java
+++ src/main/java/br/com/emerlopes/payments/infrastructure/security/SecurityService.java
@@ -1,7 +1,9 @@
 package br.com.emerlopes.payments.infrastructure.security;
 
 import br.com.emerlopes.payments.application.exceptions.InvalidTokenException;
+import br.com.emerlopes.payments.application.shared.CustomResponseDTO;
 import br.com.emerlopes.payments.infrastructure.integrations.hackathonauth.HackathonAuthClient;
+import br.com.emerlopes.payments.infrastructure.integrations.hackathonauth.dto.TokenResponseDTO;
 import org.slf4j.Logger;
 import org.springframework.stereotype.Service;
 
@@ -23,7 +25,7 @@ public class SecurityService {
     ) {
         log.info("Validando token");
 
-        final var response = hackathonAuthClient.validateToken(token);
+        final CustomResponseDTO<TokenResponseDTO> response = hackathonAuthClient.validateToken(token);
 
         log.info("Token validado");
 
