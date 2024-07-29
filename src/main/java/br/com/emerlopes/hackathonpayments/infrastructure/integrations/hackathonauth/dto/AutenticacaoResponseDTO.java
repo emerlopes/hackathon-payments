0faac4649353fda61681e2310d66d5077480df66
Emerson <lopes.ds@live.com>
2024-07-29 11:18:46 -0300

    chore: implementando integracao com o servico de autenticacao.

---
+++ src/main/java/br/com/emerlopes/hackathonpayments/infrastructure/integrations/hackathonauth/dto/AutenticacaoResponseDTO.java
@@ -0,0 +1,12 @@
+package br.com.emerlopes.hackathonpayments.infrastructure.integrations.hackathonauth.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class AutenticacaoResponseDTO {
+    @JsonProperty("token_acesso")
+    private String tokenAcesso;
+}
