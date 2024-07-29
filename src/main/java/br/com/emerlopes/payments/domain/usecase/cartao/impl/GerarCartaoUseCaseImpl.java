bfd31eb2f7e6bd678592cb503dbb361bd6d31f72
Emerson <lopes.ds@live.com>
2024-07-29 16:35:49 -0300

    chore: implementando geracao de cartao.

--- src/main/java/br/com/emerlopes/payments/domain/usecase/cartao/impl/GerarCartaoUseCaseImpl.java
+++ src/main/java/br/com/emerlopes/payments/domain/usecase/cartao/impl/GerarCartaoUseCaseImpl.java
@@ -1,7 +1,11 @@
 package br.com.emerlopes.payments.domain.usecase.cartao.impl;
 
+import br.com.emerlopes.payments.application.shared.CpfUtils;
 import br.com.emerlopes.payments.domain.entity.CartaoDomainEntity;
+import br.com.emerlopes.payments.domain.entity.ClienteDomainEntity;
+import br.com.emerlopes.payments.domain.exceptions.BusinessExceptions;
 import br.com.emerlopes.payments.domain.repository.CartaoDomainRepository;
+import br.com.emerlopes.payments.domain.repository.ClienteDomainRepository;
 import br.com.emerlopes.payments.domain.usecase.cartao.GerarCartaoUseCase;
 import org.slf4j.Logger;
 import org.springframework.stereotype.Service;
@@ -11,18 +15,49 @@ public class GerarCartaoUseCaseImpl implements GerarCartaoUseCase {
 
     private final static Logger log = org.slf4j.LoggerFactory.getLogger(GerarCartaoUseCaseImpl.class);
     private final CartaoDomainRepository cartaoDomainRepository;
+    private final ClienteDomainRepository clienteDomainRepository;
 
     public GerarCartaoUseCaseImpl(
-            final CartaoDomainRepository cartaoDomainRepository
+            final CartaoDomainRepository cartaoDomainRepository,
+            final ClienteDomainRepository clienteDomainRepository
     ) {
         this.cartaoDomainRepository = cartaoDomainRepository;
+        this.clienteDomainRepository = clienteDomainRepository;
     }
 
     @Override
     public CartaoDomainEntity execute(
             final CartaoDomainEntity cartaoDomainEntity
     ) {
-        log.info(String.format("Criando cartao para o cliente: %s", cartaoDomainEntity.getNumero()));
+
+        final String cpf = cartaoDomainEntity.getCpf();
+        final String cpfMascarado = CpfUtils.mascararCpf(cpf);
+
+        log.info("Verificando quantidade de cartoes permitidos para o cliente: {}", cpfMascarado);
+
+        final boolean jaPossuiDoisCartoes = cartaoDomainRepository.jaPossuiDoisCartoes(
+                cartaoDomainEntity
+        );
+
+        if (jaPossuiDoisCartoes) {
+            log.error("Cliente ja possui dois cartoes: {}", cpfMascarado);
+            throw new BusinessExceptions("Cliente ja possui dois cartoes");
+        }
+
+        final ClienteDomainEntity clienteDomainEntity = clienteDomainRepository.buscarClientePorCpf(
+                ClienteDomainEntity.builder()
+                        .cpf(cpf)
+                        .build()
+        );
+
+        if (clienteDomainEntity == null) {
+            log.error("Cliente nao encontrado: {}", cpfMascarado);
+            throw new BusinessExceptions("Só é possível registrar um novo cartão para um cliente já existente.");
+        }
+
+        cartaoDomainEntity.setCliente(clienteDomainEntity);
+
+        log.info("Criando cartao para o cliente: {}", cpfMascarado);
         return cartaoDomainRepository.gerarCartao(cartaoDomainEntity);
     }
 }
