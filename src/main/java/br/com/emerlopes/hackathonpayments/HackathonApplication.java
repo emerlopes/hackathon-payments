b8495a4cc313b5582e69cf002e0bc04572dfe813
Emerson <lopes.ds@live.com>
2024-07-29 11:18:20 -0300

    chore: adicionando anotacao EnableFeignClients

--- src/main/java/br/com/emerlopes/hackathonpayments/HackathonApplication.java
+++ src/main/java/br/com/emerlopes/hackathonpayments/HackathonApplication.java
@@ -2,8 +2,10 @@ package br.com.emerlopes.hackathonpayments;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients({"br.com.emerlopes.hackathonpayments.infrastructure.integrations*"})
 public class HackathonApplication {
 
     public static void main(String[] args) {
